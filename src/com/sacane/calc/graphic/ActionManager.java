70d53cce58ee8a87c51787cbcb5153dd689372b9
Johan <johan.ramaroson@gmail.com>
2021-01-30 22:59:03 +0100

    update of the design

--- src/com/sacane/calc/graphic/ActionManager.java
+++ src/com/sacane/calc/graphic/ActionManager.java
@@ -1,5 +1,6 @@
 package com.sacane.calc.graphic;
 import com.sacane.calc.evaluator.BaseOperator;
+import com.sacane.calc.evaluator.Evaluator;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -12,8 +13,12 @@ public class ActionManager implements ActionListener {
 
     private final HashSet<JButton> numberButton = new HashSet<>();
     private final HashSet<JButton> operatorButton = new HashSet<>();
+    private final JButton equalButton = new JButton("=");
+    private final JButton leftParenthesis = new JButton("(");
+    private final JButton rightParenthesis = new JButton(")");
 
     private StringBuilder builder = new StringBuilder();
+
 
 
     JPanel initButton(){
@@ -30,8 +35,31 @@ public class ActionManager implements ActionListener {
             button.addActionListener(this);
             panel.add(button);
         }
-        panel.add(new JButton("="));
+        leftParenthesis.addActionListener(this);
+        rightParenthesis.addActionListener(this);
+        equalButton.addActionListener(this);
+        panel.add(equalButton);
+        panel.add(leftParenthesis);
+        panel.add(rightParenthesis);
         return panel;
+    }
+
+    void clearBuilder(){
+        builder.delete(0, builder.length());
+    }
+
+    void display_result(String input){
+        var evaluated = Evaluator.eval(input);
+        System.out.printf("evaluating : %s%n", builder.toString());
+        if ((evaluated == Math.floor(evaluated)) && !Double.isInfinite(evaluated)) {
+
+            System.out.println("results : " + (int)evaluated);
+        }
+        else{
+            System.out.println("results :" + Evaluator.eval(input));
+        }
+
+
     }
 
     @Override
@@ -39,10 +67,24 @@ public class ActionManager implements ActionListener {
         var src = e.getSource();
         var button = (JButton)src;
         if(numberButton.contains(src)){
-            System.out.printf("Click on %s%n", button.getText());
+
+            builder.append(button.getText());
         }
         else if(operatorButton.contains(src)){
-            System.out.printf("Click on %s%n", button.getText());
+
+            builder.append(button.getText());
         }
+        else if(leftParenthesis == src) {
+            builder.append(leftParenthesis.getText());
+
+        } else if(rightParenthesis == src){
+            builder.append(rightParenthesis.getText());
+        }
+        else if(src == equalButton){
+
+            display_result(builder.toString());
+            clearBuilder();
+        }
+
     }
 }
